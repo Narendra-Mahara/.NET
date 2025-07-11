53a2ce3e1b02957072ec019bd3290f8bf2c35090
Narendra Mahara <narendramahara9868@gmail.com>
2025-07-11 20:11:46 +0545

    Add initial implementation of ReadLine application

---
+++ ReadLine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+           string upperCaseName= name.ToUpper();
+            Console.WriteLine("Your name is "+ upperCaseName);
+        }
+    }
+}
