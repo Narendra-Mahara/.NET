ca7d3c9c7e6eb732192b0f070fa598bb0f63cfbe
Narendra Mahara <narendramahara9868@gmail.com>
2025-07-30 23:13:50 +0545

    Add basic data types of c#

---
+++ DataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Narendra Mahara";
+            int age = 23;
+            bool isSingle = true;
+            float bankBalance = 3037.29F;
+            Console.WriteLine("Hello! my name is " + name + " and my age is " + age+". My bank balance is RS."+bankBalance);
+            if (isSingle)
+            {
+                Console.WriteLine("I am single 😄");
+            }
+
+        }
+    }
+}
+
