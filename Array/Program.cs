a1c4dd9b70346ef6cabfd856cc36f1a53df9e7e1
Narendra Mahara <narendramahara9868@gmail.com>
2025-07-30 23:49:13 +0545

    Learn 1D array

---
+++ Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marksObtained = [55, 60, 35, 42, 50];
+
+            // for (int i = 0; i < marksObtained.Length; i++)
+            // {
+
+            //     Console.WriteLine(marksObtained[i]);
+            // }
+            
+            //another loop to access elements of array
+            foreach (int marks in marksObtained)
+            {
+                Console.WriteLine(marks);
+            }
+
+        }
+    }
+}
+
