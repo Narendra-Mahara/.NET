85d33580a08cb512b3a2dba0e685c223fddcc481
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-01 20:51:21 +0545

    Add initial project and program files for Methods and Functions implementation

---
+++ Methods(Functions)/Program.cs
@@ -0,0 +1,27 @@
+﻿// A method is a block of code which only runs when it is called.
+using System;
+
+
+namespace Methods
+{
+    static class Program
+    {
+        static void MyMethod(string name="K")  //default value
+        {
+            Console.WriteLine("My name is : "+name);
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            MyMethod("Narendra"); //passing data, known as parameters, into a method.
+            MyMethod("S");
+
+            int value = Add(19, 29);
+            Console.WriteLine("The sum of 19 and 29 is :" + value);        }
+    }
+}
+
