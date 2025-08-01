447cac949948f101ebba8afd44056afcabf1c014
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-01 20:59:07 +0545

    Add initial implementation of String project with example usage of string interpolation and methods

---
+++ String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Narendra Mahara";
+            int age = 23;
+            float bankBalance = 3037.29F;
+            // Console.WriteLine("Hello! my name is " + name + " and my age is " + age+". My bank balance is RS."+bankBalance);
+
+
+            // string interpolation
+            Console.WriteLine($"Hello! my name is{name} and my age is {age}. My bank balance is RS.{bankBalance}");
+
+            // string length
+            Console.WriteLine(name.Length);
+
+            //Indexof and Subtring
+
+            int charPos = name.IndexOf("d");
+            string substr = name.Substring(charPos);  // Substring return the part of the string starting from the given position
+            Console.WriteLine(substr); 
+            
+
+
+        }
+    }
+}
+
