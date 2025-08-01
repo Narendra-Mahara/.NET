fb7e95de6c7a3b4b26ee92f0d068d783d55d31ed
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-01 20:33:27 +0545

    Add initial implementation of MultiDimensionalArray project with example usage

---
+++ MultiDimensionalArray/Program.cs
@@ -0,0 +1,27 @@
+﻿// A multidimensional array is basically an array of arrays.
+//  The single comma [,] specifies that the array is two-dimensional. A three-dimensional array would have two commas: int[,,].
+
+using System;
+
+
+namespace MultiDimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] superHeroes = { { "Thor", "SpiderMan" }, { "Batman", "Narendra" } };
+            // Console.WriteLine(superHeroes[1, 1]);  //First value is the array and second value is the position of the element inside that array
+
+            // Console.WriteLine(superHeroes.GetLength(0)); // to get the length of the array inside the two-dimensional array
+
+            //We can iterate through the two-dimensional array using foreach loop
+            foreach (string superHero in superHeroes)
+            {
+                Console.WriteLine(superHero);
+            } 
+
+        }   
+    }
+}
+
