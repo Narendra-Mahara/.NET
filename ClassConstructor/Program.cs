f22c635f1218a23974bc40ea978362140d5e37f7
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-04 09:37:57 +0545

    Add initial implementation of ClassConstructor project with Car class and constructor example

---
+++ ClassConstructor/Program.cs
@@ -0,0 +1,25 @@
+﻿// Constructor is special method used to initilaize the object. It is automatically called when object is created. It is used to set the initial value of the fields.
+// The name of constructor should be same as the class name but without return type (void,int)
+namespace Constructors
+{
+    class Car
+    {
+        public string color;
+        public Car(string carColor)
+        {
+            color = carColor;
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine("The color of my 🚗 is: "+color);
+        }
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Red");
+            // Console.WriteLine(myCar.color);
+            myCar.ShowDetails();
+        }
+    }
+}
+
