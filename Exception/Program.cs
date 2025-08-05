8c7d90b426b9cfb93498c609a5bdc518c9315199
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-05 23:59:50 +0545

    Add initial implementation of Exception handling with Program class

---
+++ Exception/Program.cs
@@ -0,0 +1,15 @@
+﻿// This code demonstrates how to handle exceptions in C#.
+// try and catch blocks are used to catch exceptions that may occur during the execution of code.
+//finally block can be used to execute code regardless of whether an exception occurred or not.
+try
+{
+    int[] myNumbers = { 1, 2, 3 };
+    Console.WriteLine(myNumbers[10]);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Error: "+e.Message);
+}finally
+{
+  Console.WriteLine("The 'try catch' is finished.");
+}
