cfddb99962334983ac6137286d09124a42612542
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-05 23:47:07 +0545

    Add initial implementation of Enums project with Level enum and Program class

---
+++ ENums/Program.cs
@@ -0,0 +1,26 @@
+﻿// An enum is a special "class" that represents a group of constants (unchangeable/read-only variables).
+// We can access enum items with the dot(.) syntax:
+// By default, the first item of an enum has the value 0. The second has the value 1, and so on.
+// Use enums when you have values that you know aren't going to change, like month days, days, colors, deck of cards, etc.
+
+
+
+namespace Enums
+{
+    enum Level
+    {
+        Low=8,
+        Medium, // 9
+        High // 10
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar =(int) Level.Medium;
+            Console.WriteLine(myVar);
+        }
+    }
+
+}
