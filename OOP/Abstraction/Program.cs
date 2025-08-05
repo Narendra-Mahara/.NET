79aeb6d1ffef3497647cd7f2a0e13c1b49a4e838
Narendra Mahara <narendramahara9868@gmail.com>
2025-08-05 23:10:12 +0545

    Add initial implementation of Abstraction project with Animal and Cat classes

---
+++ OOP/Abstraction/Program.cs
@@ -0,0 +1,34 @@
+﻿// Abstraction in C# is a fundamental concept that allows us to hide complex implementation details and expose only the necessary parts of an object.
+// We have to use abstract keyword to define an abstract class or method.
+// An abstract class cannot be instantiated (we cannot create an instance of it) directly and can contain abstract methods that must be implemented by derived classes.
+
+
+
+abstract class Animal
+{
+    public abstract void animalSound();
+    public void sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+
+class Cat : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The cat says: meow meow");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Animal dog = new Animal(); //  Cannot create an instance of the abstract type or interface 'Animal'
+
+         Cat cat = new Cat(); // Only accessible through derived class
+        cat.animalSound();
+    }
+}
